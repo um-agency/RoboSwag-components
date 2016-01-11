ef86c1931135a48a71647d5e046c459a264a2da3
Gavriil Sitnikov <zuzuk666@gmail.com>
2016-01-11 16:11:23 +0300

    viewcontroller added

---
+++ src/main/java/org/roboswag/components/navigation/ViewFragment.java
@@ -0,0 +1,197 @@
+/*
+ *  Copyright (c) 2015 RoboSwag (Gavriil Sitnikov, Vsevolod Ivanov)
+ *
+ *  This file is part of RoboSwag library.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.roboswag.components.navigation;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.roboswag.core.log.Lc;
+
+import rx.functions.Action2;
+
+/**
+ * Created by Gavriil Sitnikov on 21/10/2015.
+ * Fragment that have specific activity as a parent and can't be background.
+ */
+public abstract class ViewFragment<TActivity extends AppCompatActivity> extends Fragment {
+
+    /**
+     * Returns if fragment have parent fragment.
+     *
+     * @return Returns true if fragment is in some fragment's children stack.
+     */
+    public final boolean isChildFragment() {
+        return getParentFragment() != null || getTargetFragment() != null;
+    }
+
+    /**
+     * Returns specific activity which to this fragment could be attached.
+     *
+     * @return Returns parent activity.
+     */
+    @SuppressWarnings("unchecked")
+    @Nullable
+    protected TActivity getBaseActivity() {
+        if (getActivity() == null) {
+            return null;
+        }
+
+        try {
+            return (TActivity) getActivity();
+        } catch (ClassCastException exception) {
+            Lc.assertion(exception);
+            return null;
+        }
+    }
+
+    @Deprecated
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater,
+                             @Nullable final ViewGroup container,
+                             @Nullable final Bundle savedInstanceState) {
+        throw new IllegalStateException("Method onCreateView() should be override.");
+    }
+
+    @Deprecated
+    @Override
+    public void onActivityCreated(@Nullable final Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getView() != null && getBaseActivity() != null) {
+            onActivityCreated(getView(), getBaseActivity(), savedInstanceState);
+        } else {
+            Lc.assertion("View and activity shouldn't be null");
+        }
+    }
+
+    /**
+     * Replacement of {@link #onActivityCreated} with non null activity as first parameter.
+     *
+     * @param view               Instantiated view.
+     * @param activity           Activity which fragment attached to.
+     * @param savedInstanceState If the fragment is being re-created from a previous saved state, this is the state.
+     */
+    public void onActivityCreated(@NonNull final View view, @NonNull final TActivity activity, @Nullable final Bundle savedInstanceState) {
+        //do nothing
+    }
+
+    private void callMethodAfterInstantiation(@NonNull final Action2<View, TActivity> action) {
+        if (getView() != null && getBaseActivity() != null) {
+            action.call(getView(), getBaseActivity());
+        } else {
+            Lc.assertion("View and activity shouldn't be null");
+        }
+    }
+
+    @Deprecated
+    @Override
+    public void onStart() {
+        super.onStart();
+        callMethodAfterInstantiation(this::onStart);
+    }
+
+    /**
+     * Replacement of {@link #onStart} with non null activity as first parameter.
+     *
+     * @param view     Instantiated view.
+     * @param activity Activity which fragment attached to.
+     */
+    protected void onStart(@NonNull final View view, @NonNull final TActivity activity) {
+        //do nothing
+    }
+
+    @Deprecated
+    @Override
+    public void onResume() {
+        super.onResume();
+        callMethodAfterInstantiation(this::onResume);
+    }
+
+    /**
+     * Replacement of {@link #onResume} with non null activity as first parameter.
+     *
+     * @param view     Instantiated view.
+     * @param activity Activity which fragment attached to.
+     */
+    protected void onResume(@NonNull final View view, @NonNull final TActivity activity) {
+        //do nothing
+    }
+
+    @Deprecated
+    @Override
+    public void onPause() {
+        callMethodAfterInstantiation(this::onPause);
+        super.onPause();
+    }
+
+    /**
+     * Replacement of {@link #onPause} with non null activity as first parameter.
+     *
+     * @param view     Instantiated view.
+     * @param activity Activity which fragment attached to.
+     */
+    protected void onPause(@NonNull final View view, @NonNull final TActivity activity) {
+        //do nothing
+    }
+
+    @Deprecated
+    @Override
+    public void onStop() {
+        callMethodAfterInstantiation(this::onStop);
+        super.onStop();
+    }
+
+    /**
+     * Replacement of {@link #onStop} with non null activity as first parameter.
+     *
+     * @param view     Instantiated view.
+     * @param activity Activity which fragment attached to.
+     */
+    protected void onStop(@NonNull final View view, @NonNull final TActivity activity) {
+        //do nothing
+    }
+
+    @Deprecated
+    @Override
+    public void onDestroyView() {
+        if (getView() != null) {
+            onDestroyView(getView());
+        } else {
+            Lc.assertion("View shouldn't be null");
+        }
+        super.onDestroyView();
+    }
+
+    /**
+     * Replacement of {@link #onDestroyView} with non null activity as first parameter.
+     *
+     * @param view Instantiated view.
+     */
+    protected void onDestroyView(@NonNull final View view) {
+        //do nothing
+    }
+
+}
---
+++ src/main/java/org/roboswag/components/services/LogicService.java
@@ -0,0 +1,48 @@
+package org.roboswag.components.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.IBinder;
+
+import org.roboswag.core.utils.android.ServiceBinder;
+
+/**
+ * Created by Gavriil Sitnikov on 10/01/2016.
+ * Service which holds interface to all application's logic objects and methods.
+ * Any part of application should interact with some part of logic via this interface.
+ * If it is Service, Activity, Fragment or iew then it should bind itself to that service first then get service and logic bridge from IBinder.
+ * If it is BroadcastReceiver then it should start service which can bind to that service or just send some intent to that service.
+ */
+public abstract class LogicService<TLogicBridge> extends IntentService {
+
+    private final TLogicBridge logicBridge;
+
+    public LogicService(final String name) {
+        super(name);
+        this.logicBridge = createLogicBridge();
+    }
+
+    /**
+     * Creates object which will provide all logic methods and objects of application.
+     * Any other activity, fragment or service should bind to that service to get logic bridge and start interact with application's logic.
+     * Do not initialize massive objects during creation as this operation is calling on UI thread.
+     *
+     * @return Returns instantiated logic bridge.
+     */
+    protected abstract TLogicBridge createLogicBridge();
+
+    /**
+     * Returns interface to application's logic.
+     *
+     * @return Returns logic bridge object.
+     */
+    public TLogicBridge getLogicBridge() {
+        return logicBridge;
+    }
+
+    @Override
+    public final IBinder onBind(final Intent intent) {
+        return new ServiceBinder<>(this);
+    }
+
+}
