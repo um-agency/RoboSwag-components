8289a22e76f767084007c984acf2757b491694cb
ilia.kurtov <kurtovi@gmail.com>
2016-01-15 03:46:45 +0300

    rename classes

--- src/main/java/org/roboswag/components/utils/PermissionState.java
+++ src/main/java/org/roboswag/components/utils/PermissionState.java
@@ -3,8 +3,8 @@ package org.roboswag.components.utils;
 /**
  * Created by Ilia Kurtov on 14.01.2016.
  */
-public enum PermissionsAnswer {
+public enum PermissionState {
     GRANTED,
-    DENIED_PREVIOUSLY,
-    DENIED
+    DENIED_THIS_TIME,
+    DENIED_COMPLETELY
 }
