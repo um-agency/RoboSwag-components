44a9049752462b2d96b6bc79238ccf79be2d0ed7
Ilia Kurtov <ilia.kurtov@touchin.ru>
2016-02-02 15:55:37 +0300

    add annotation import

--- src/main/java/org/roboswag/components/utils/UiUtils.java
+++ src/main/java/org/roboswag/components/utils/UiUtils.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.util.DisplayMetrics;
