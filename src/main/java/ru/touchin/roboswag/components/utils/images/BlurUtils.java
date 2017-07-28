26dbe3701429cc66c1dbbb3710a6e186fad29436
Gavriil Sitnikov <zuzuk666@gmail.com>
2017-07-28 11:24:57 +0300

    Blur recycle fix

--- src/main/java/ru/touchin/roboswag/components/utils/images/BlurUtils.java
+++ src/main/java/ru/touchin/roboswag/components/utils/images/BlurUtils.java
@@ -31,13 +31,16 @@ public final class BlurUtils {
     @Nullable
     public static Bitmap blurRenderscript(@NonNull final Context context, @NonNull final Bitmap bitmap, final int radius) throws RSRuntimeException {
         RenderScript rs = null;
+        Allocation input = null;
+        Allocation output = null;
+        ScriptIntrinsicBlur blur = null;
         try {
             rs = RenderScript.create(context);
-            final Allocation input =
-                    Allocation.createFromBitmap(rs, bitmap, Allocation.MipmapControl.MIPMAP_NONE,
-                            Allocation.USAGE_SCRIPT);
-            final Allocation output = Allocation.createTyped(rs, input.getType());
-            final ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+            rs.setMessageHandler(new RenderScript.RSMessageHandler());
+            input = Allocation.createFromBitmap(rs, bitmap, Allocation.MipmapControl.MIPMAP_NONE,
+                    Allocation.USAGE_SCRIPT);
+            output = Allocation.createTyped(rs, input.getType());
+            blur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
 
             blur.setInput(input);
             blur.setRadius(radius);
@@ -46,6 +49,15 @@ public final class BlurUtils {
         } finally {
             if (rs != null) {
                 rs.destroy();
+            }
+            if (input != null) {
+                input.destroy();
+            }
+            if (output != null) {
+                output.destroy();
+            }
+            if (blur != null) {
+                blur.destroy();
             }
         }
 
